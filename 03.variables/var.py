b7402b63d7070321483296c1b4cc80a814f2b36e
Marcelo Júnior <rokermarcelo@gmail.com>
2022-09-09 11:03:11 -0300

    Comentários complementares ao uso de variáveis

--- 03.variables/var.py
+++ 03.variables/var.py
@@ -10,6 +10,8 @@ várias linhas."""
 print(s)
 
 # Caso você possua uma instrução muito longa, é possível fazer a quebra de linha mantendo a instrução com o uso da barra invertida "\"
+# Também é válido para quebrar strings
 i = \
 85
+# As linhas anteriores equivalem a: i = 85
 print(i)

e228896beb3b5fe802cdb5dae84f4cd8957d766c
Marcelo Júnior <rokermarcelo@gmail.com>
2022-09-09 11:01:40 -0300

    Uso básico de variáveis

---
+++ 03.variables/var.py
@@ -0,0 +1,15 @@
+# Variáveis podem ter seus valores modificados
+i = 5
+print(i)
+i = i +1
+print(i)
+
+# É possível criar strigs com múltiplas linhas ao usar três aspas (simples ou duplas)
+s = """Esta é uma frase formada por:
+várias linhas."""
+print(s)
+
+# Caso você possua uma instrução muito longa, é possível fazer a quebra de linha mantendo a instrução com o uso da barra invertida "\"
+i = \
+85
+print(i)
